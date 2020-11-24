e84baa3ce9f88aaeb283da03f8822cfea5ad018c
William <william.dzierzynski@outlook.fr>
2020-11-24 11:01:35 +0100

    Serveur avec Express //Détails possibles

--- serveur.js
+++ serveur.js
@@ -18,36 +18,40 @@
 let express = require('express');
 let serveur = express();
 
+serveur.use(express.urlencoded({ extended: false }));
+
 serveur.get('/', (req, res) => {
-    // console.log("requete sur '/'");
-    res.send(`
-        <form action="/reponse" method="POST">
-        <p>Combien font 2 + 2 ?</p>
-        <input name="reponseDonnee" autocomplete="off">
-        <button>envoyer</button>
-        </form>
-    `);
+  //console.log(`requête sur '/'`);
+  res.send(`
+    <form action="/reponse" method="POST">
+      <p>Combien font 2 + 2 ?</p>
+      <input name="reponseDonnee" autocomplete="off">
+      <button>Envoyer</button>
+    </form>
+  `);
 });
 
 serveur.post('/reponse', function (req, res) {
-    //res.send('Bien reçu!');
-    //résupérer la valeur du formulaire;
-    //console.log(req.body, reponseDonnee);
-    if (req.body.reponseDonnee === '4') {
-    res.send ( `Bravo ! <br>
-    <a href='/'>Retour à l accueil</a>` );
-    } else {
-    res.send ( `Perdu ! <br> 
-    <a href='/'>Retour à l accuei</a>` );
-    }
+  //res.send('Bien reçu');
+  // recuperer la valeur du formulaire
+  //console.log(req.body.reponseDonnee);
+  if (req.body.reponseDonnee === '4') {
+    res.send(`Bravo ! <br>
+    <a href='/'>Retour à l'accueil</a>`);
+  } else {
+    res.send(`Perdu ! <br>
+    <a href='/'>Retour à l'accueil</a>`);
+  }
 });
 
-serveur.get('/reponse', (req,res) => {
-    res.send('Etes-vous perdu?');
+serveur.get('/reponse', (req, res) => {
+  res.send('Êtes-vous perdu ?');
 })
 
+
+// camelCase => premiereLettreEnMinuscule
+// PascalCase => PremiereLettreEnMajuscule
+// snake_case => mots_separes_par_des_tirets
+
 serveur.listen(3000);
-console.log("écoute sur port 3000");
-
-
-console.log("Coucou !");
+console.log('ecoute sur port 3000...');
